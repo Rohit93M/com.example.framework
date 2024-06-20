da5f5da2fcc00bd15a45b8788b6e5e289e35c74a
rohit <rohit93m@gmail.com>
2024-06-20 14:20:50 +0530

    modified second test method in class TrelloAppTest

--- src/main/java/pomrepo/TrelloTopicPage.java
+++ src/main/java/pomrepo/TrelloTopicPage.java
@@ -6,64 +6,172 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class TrelloTopicPage {
-    
-    WebDriver driver;
 
-    public TrelloTopicPage(WebDriver driver) {
-        this.driver = driver;
-        PageFactory.initElements(driver, this);
-    }
+	WebDriver driver;
 
-    @FindBy(xpath = "//button[@aria-label='Show menu']")
-    WebElement threeDots;
+	public TrelloTopicPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
 
-    @FindBy(xpath = "//a[@class='board-menu-navigation-item-link board-menu-navigation-item-link-v2 js-close-board']")
-    WebElement closeBoardOption;
+	@FindBy(xpath = "//button[@aria-label='Show menu']")
+	WebElement threeDots;
 
-    @FindBy(xpath = "//input[@value='Close']")
-    WebElement closeOption;
+	@FindBy(xpath = "//a[@class='board-menu-navigation-item-link board-menu-navigation-item-link-v2 js-close-board']")
+	WebElement closeBoardOption;
 
-    @FindBy(xpath = "//button[normalize-space()='Permanently delete board']")
-    WebElement permanentlyDeleteBoardOpt;
+	@FindBy(xpath = "//input[@value='Close']")
+	WebElement closeOption;
 
-    @FindBy(xpath = "//button[normalize-space()='Delete']")
-    WebElement deleteOption;
+	@FindBy(xpath = "//button[normalize-space()='Permanently delete board']")
+	WebElement permanentlyDeleteBoardOpt;
 
-    @FindBy(xpath = "//button[normalize-space()='Add a list']")
-    WebElement addListButton;
+	@FindBy(xpath = "//button[normalize-space()='Delete']")
+	WebElement deleteOption;
 
-    @FindBy(xpath = "//textarea[@placeholder='Enter list title…']")
-    WebElement listTitleTextField;
+	@FindBy(xpath = "(//textarea[@data-testid='list-name-textarea'])[1]")
+	WebElement listNameTextarea1;
 
-    @FindBy(xpath = "//input[@value='Add list']")
-    WebElement saveListButton;
+	@FindBy(xpath = "(//textarea[@data-testid='list-name-textarea'])[2]")
+	WebElement listNameTextarea2;
 
-    @FindBy(xpath = "//button[normalize-space()='Add a card']")
-    WebElement addCardButton;
+	@FindBy(xpath = "(//textarea[@data-testid='list-name-textarea'])[3]")
+	WebElement listNameTextarea3;
 
-    @FindBy(xpath = "//textarea[@placeholder='Enter a title for this card…']")
-    WebElement cardTitleTextField;
+	@FindBy(xpath = "(//textarea[@data-testid='list-name-textarea'])[4]")
+	WebElement listNameTextarea4;
 
-    @FindBy(xpath = "//input[@value='Add card']")
-    WebElement saveCardButton;
+	@FindBy(xpath = "//button[@type='submit']")
+	WebElement submitButton;
 
-    public WebElement getThreeDots() {
-        return threeDots;
-    }
+	@FindBy(xpath = "//button[text()='Add a card']")
+	WebElement addCardButton;
 
-    public WebElement getCloseBoardOption() {
-        return closeBoardOption;
-    }
+	@FindBy(xpath = "//textarea[@placeholder='Enter a title for this card…']")
+	WebElement cardTitleTextarea;
 
-    public WebElement getCloseOption() {
-        return closeOption;
-    }
+	@FindBy(xpath = "//button[text()='Add card']")
+	WebElement addCardSubmitButton;
 
-    public WebElement getPermanentlyDeleteBoardOpt() {
-        return permanentlyDeleteBoardOpt;
-    }
+	@FindBy(xpath = "//span[@data-testid='CloseIcon']")
+	WebElement closeIcon;
 
-    public WebElement getDeleteOption() {
-        return deleteOption;
-    }
+	@FindBy(xpath = "//a[text()='Test Cases']")
+	WebElement testCaseTopic;
+
+	@FindBy(xpath = "//a[text()='Agile Methodology']")
+	WebElement agileTopic;
+
+	@FindBy(xpath = "//h2[text()='Manual']")
+	WebElement manualTextField;
+
+	@FindBy(xpath = "//a[text()='Joins']")
+	WebElement joinsTopic;
+
+	@FindBy(xpath = "//a[text()='Normalization']")
+	WebElement normalizationTopic;
+
+	@FindBy(xpath = "//h2[text()='SQL']")
+	WebElement sqlTextField;
+
+	@FindBy(xpath = "//a[text()='OOPS']")
+	WebElement oopsTopic;
+
+	@FindBy(xpath = "//a[text()='Collection']")
+	WebElement collectionTopic;
+
+	@FindBy(xpath = "//h2[text()='Java']")
+	WebElement javaTextField;
+
+	public WebElement getThreeDots() {
+		return threeDots;
+	}
+
+	public WebElement getCloseBoardOption() {
+		return closeBoardOption;
+	}
+
+	public WebElement getCloseOption() {
+		return closeOption;
+	}
+
+	public WebElement getPermanentlyDeleteBoardOpt() {
+		return permanentlyDeleteBoardOpt;
+	}
+
+	public WebElement getDeleteOption() {
+		return deleteOption;
+	}
+
+	public WebElement getListNameTextarea1() {
+		return listNameTextarea1;
+	}
+
+	public WebElement getListNameTextarea2() {
+		return listNameTextarea2;
+	}
+
+	public WebElement getListNameTextarea3() {
+		return listNameTextarea3;
+	}
+
+	public WebElement getListNameTextarea4() {
+		return listNameTextarea4;
+	}
+
+	public WebElement getSubmitButton() {
+		return submitButton;
+	}
+
+	public WebElement getAddCardButton() {
+		return addCardButton;
+	}
+
+	public WebElement getCardTitleTextarea() {
+		return cardTitleTextarea;
+	}
+
+	public WebElement getAddCardSubmitButton() {
+		return addCardSubmitButton;
+	}
+
+	public WebElement getCloseIcon() {
+		return closeIcon;
+	}
+
+	public WebElement getTestCaseTopic() {
+		return testCaseTopic;
+	}
+
+	public WebElement getAgileTopic() {
+		return agileTopic;
+	}
+
+	public WebElement getManualTextField() {
+		return manualTextField;
+	}
+
+	public WebElement getJoinsTopic() {
+		return joinsTopic;
+	}
+
+	public WebElement getNormalizationTopic() {
+		return normalizationTopic;
+	}
+
+	public WebElement getSqlTextField() {
+		return sqlTextField;
+	}
+
+	public WebElement getOopsTopic() {
+		return oopsTopic;
+	}
+
+	public WebElement getCollectionTopic() {
+		return collectionTopic;
+	}
+
+	public WebElement getJavaTextField() {
+		return javaTextField;
+	}
 }
--- src/test/java/trelloapp_test/TrelloAppTest.java
+++ src/test/java/trelloapp_test/TrelloAppTest.java
@@ -1,14 +1,8 @@
 package trelloapp_test;
 
 import java.io.IOException;
-import java.time.Duration;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.ElementNotInteractableException;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
@@ -117,148 +111,147 @@ public class TrelloAppTest extends BaseClass {
 	@Test
 	public void writeAScriptToCheckClickHoldAndReleaseFunctionalityWithCardsAndListsInTrelloBoardsPage() {
 
-		try {
+		 try {
+	        	
+	            TrelloWelcomePage welcomePage = new TrelloWelcomePage(driver);
+	            TrelloLoginPage loginPage = new TrelloLoginPage(driver);
+	            TrelloBoardsPage boardsPage = new TrelloBoardsPage(driver);
+	            TrelloTopicPage topicPage = new TrelloTopicPage(driver);
+	            TrelloLogoutPage logoutPage = new TrelloLogoutPage(driver);
 
-			WebElement loginLink = driver.findElement(By.xpath("//a[text()='Log in']"));
-			loginLink.click();
-			Actions actions = new Actions(driver);
-			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+	            webdriverUtils.waitForATitle(driver, "Manage Your Team’s Projects From Anywhere | Trello");
+	            Assert.assertEquals(driver.getTitle(), "Manage Your Team’s Projects From Anywhere | Trello",
+	                    "Welcome page title does not match");
 
-			// V&V Login page ->
-			wait.until(ExpectedConditions.titleIs("Log in to continue - Log in with Atlassian account"));
-			String loginPageActualTitle = driver.getTitle();
-			String loginPageExpectedTitle = "Log in to continue - Log in with Atlassian account";
-			if (loginPageActualTitle.equals(loginPageExpectedTitle)) {
-				WebElement emailTextField = driver.findElement(By.id("username"));
-				emailTextField.sendKeys("rohit93m@gmail.com");
-				WebElement continueButton = driver.findElement(By.xpath("//span[text()='Continue']"));
-				continueButton.submit();
-				WebElement passwordTextField = driver.findElement(By.id("password"));
-				passwordTextField.sendKeys("Rohit@1993");
-				WebElement loginButton = driver.findElement(By.xpath("//span[text()='Log in']"));
-				loginButton.submit();
+	            webdriverUtils.waitTillElementToBeClickable(driver, welcomePage.getLoginLink());
+	            welcomePage.getLoginLink().click();
 
-				// V&V Trello Boards Page ->
-				wait.until(ExpectedConditions.titleIs("Boards | Trello"));
-				String actualBoardsPageTitle = driver.getTitle();
-				String expectedBoardsPageTitle = "Boards | Trello";
-				if (actualBoardsPageTitle.equals(expectedBoardsPageTitle)) {
-					System.out.println("Actual Title of Trello Boards Page is same as Expected Title");
-					WebElement createNewBoardOption = driver.findElement(By.xpath("//span[text()='Create new board']"));
-					createNewBoardOption.click();
-					WebElement boardTitleTextField = driver.findElement(By.xpath("//input[@type='text']"));
-					boardTitleTextField.sendKeys("Selenium");
-					WebElement createOption = driver.findElement(By.xpath("//button[text()='Create']"));
-					wait.until(ExpectedConditions.elementToBeClickable(createOption));
-					if (createOption.isEnabled()) {
-						createOption.click();
-						System.out.println("createButton is enabled");
+	            webdriverUtils.waitForATitle(driver, "Log in to continue - Log in with Atlassian account");
+	            Assert.assertEquals(driver.getTitle(), "Log in to continue - Log in with Atlassian account",
+	                    "Login page title does not match");
 
-						// V&V Chosen Title Page ->
-						wait.until(ExpectedConditions.titleIs("Selenium | Trello"));
-						String actualChosenTitle = driver.getTitle();
-						String expectedChosenTitle = "Selenium | Trello";
-						if (actualChosenTitle.equals(expectedChosenTitle)) {
-							System.out.println("actualChosenTitle is same as expectedChosenTitle");
+	            webdriverUtils.waitTillElementToBeVisible(driver, loginPage.getUsernameTextField());
+	            loginPage.getUsernameTextField().sendKeys(fileUtils.getPropertyKeyValue("email"));
 
-							// Creating Subject Data ->
-							driver.findElement(By.xpath("(//textarea[@data-testid='list-name-textarea'])[1]"))
-									.sendKeys("Topics");
-							driver.findElement(By.xpath("//button[@type='submit']")).submit();
-							driver.findElement(By.xpath("(//textarea[@data-testid='list-name-textarea'])[2]"))
-									.sendKeys("Manual");
-							driver.findElement(By.xpath("//button[@type='submit']")).submit();
-							driver.findElement(By.xpath("(//textarea[@data-testid='list-name-textarea'])[3]"))
-									.sendKeys("SQL");
-							driver.findElement(By.xpath("//button[@type='submit']")).submit();
-							driver.findElement(By.xpath("(//textarea[@data-testid='list-name-textarea'])[4]"))
-									.sendKeys("Java");
-							driver.findElement(By.xpath("//button[@type='submit']")).submit();
-							driver.findElement(By.xpath("//span[@data-testid='CloseIcon']")).click();
+	            webdriverUtils.waitTillElementToBeClickable(driver, loginPage.getContinueButton());
+	            loginPage.getContinueButton().click();
 
-							// Entering Topics Data ->
-							driver.findElement(By.xpath("//button[text()='Add a card']")).click();
-							driver.findElement(By.xpath("//textarea[@placeholder='Enter a title for this card…']"))
-									.sendKeys("Test Cases");
-							driver.findElement(By.xpath("//button[text()='Add card']")).click();
-							driver.findElement(By.xpath("//textarea[@placeholder='Enter a title for this card…']"))
-									.sendKeys("Agile Methodology");
-							driver.findElement(By.xpath("//button[text()='Add card']")).click();
-							driver.findElement(By.xpath("//textarea[@placeholder='Enter a title for this card…']"))
-									.sendKeys("Joins");
-							driver.findElement(By.xpath("//button[text()='Add card']")).click();
-							driver.findElement(By.xpath("//textarea[@placeholder='Enter a title for this card…']"))
-									.sendKeys("Normalization");
-							driver.findElement(By.xpath("//button[text()='Add card']")).click();
-							driver.findElement(By.xpath("//textarea[@placeholder='Enter a title for this card…']"))
-									.sendKeys("OOPS");
-							driver.findElement(By.xpath("//button[text()='Add card']")).click();
-							driver.findElement(By.xpath("//textarea[@placeholder='Enter a title for this card…']"))
-									.sendKeys("Collection");
-							driver.findElement(By.xpath("//button[text()='Add card']")).click();
-							driver.findElement(By.xpath("//span[@data-testid='CloseIcon']")).click();
+	            webdriverUtils.waitTillElementToBeVisible(driver, loginPage.getPasswordTextField());
+	            loginPage.getPasswordTextField().sendKeys(fileUtils.getPropertyKeyValue("password"));
 
-							// Click And Hold Action ->
-							WebElement testCaseTopic = driver.findElement(By.xpath("//a[text()='Test Cases']"));
-							WebElement agileTopic = driver.findElement(By.xpath("//a[text()='Agile Methodology']"));
-							WebElement manualTextField = driver.findElement(By.xpath("//h2[text()='Manual']"));
-							actions.clickAndHold(testCaseTopic).release(manualTextField).perform();
-							actions.clickAndHold(agileTopic).release(manualTextField).perform();
+	            webdriverUtils.waitTillElementToBeClickable(driver, loginPage.getLoginButton());
+	            loginPage.getLoginButton().click();
 
-							WebElement joinsTopic = driver.findElement(By.xpath("//a[text()='Joins']"));
-							WebElement normalizationTopic = driver.findElement(By.xpath("//a[text()='Normalization']"));
-							WebElement sqlTextField = driver.findElement(By.xpath("//h2[text()='SQL']"));
-							actions.clickAndHold(joinsTopic).release(sqlTextField).perform();
-							actions.clickAndHold(normalizationTopic).release(sqlTextField).perform();
+	            webdriverUtils.waitForATitle(driver, "Boards | Trello");
+	            Assert.assertEquals(driver.getTitle(), "Boards | Trello", "Boards page title does not match");
 
-							WebElement oopsTopic = driver.findElement(By.xpath("//a[text()='OOPS']"));
-							WebElement stringTopic = driver.findElement(By.xpath("//a[text()='Collection']"));
-							WebElement javaTextField = driver.findElement(By.xpath("//h2[text()='Java']"));
-							actions.clickAndHold(oopsTopic).release(javaTextField).perform();
-							actions.clickAndHold(stringTopic).release(javaTextField).perform();
-							System.out.println("Click and Hold Successfully Performed");
+	            webdriverUtils.waitTillElementToBeClickable(driver, boardsPage.getCreateNewBoardOption());
+	            boardsPage.getCreateNewBoardOption().click();
 
-							// Clicking On Menu Icon and Logout Link ->
-							driver.findElement(By.xpath("//div[@data-testid='header-member-menu-avatar']//span"))
-									.click();
-							WebElement logoutLink = driver.findElement(By.xpath("//span[text()='Log out']"));
-							actions.moveToElement(logoutLink).click().perform();
+	            webdriverUtils.waitTillElementToBeVisible(driver, boardsPage.getBoardTitleTextField());
+	            boardsPage.getBoardTitleTextField().sendKeys("Selenium");
+	            Assert.assertTrue(boardsPage.getCreateOption().isEnabled(), "Create Option is disabled");
 
-							// V&V Logout page and Logging out ->
-							wait.until(ExpectedConditions
-									.titleIs("Log out of your Atlassian account - Log in with Atlassian account"));
-							String logoutPageActualTitle = driver.getTitle();
-							String logoutPageExpectedTitle = "Log out of your Atlassian account - Log in with Atlassian account";
-							if (actualBoardsPageTitle.equals(expectedBoardsPageTitle)) {
-								WebElement logoutButton = driver.findElement(By.xpath("//button[@type='submit']"));
-								logoutButton.submit();
+	            webdriverUtils.waitTillElementToBeClickable(driver, boardsPage.getCreateOption());
+	            boardsPage.getCreateOption().click();
 
-								// V&V Trello HomePage ->
-								wait.until(ExpectedConditions
-										.titleIs("Manage Your Team’s Projects From Anywhere | Trello"));
-								String actualHomepageTitle = driver.getTitle();
-								String expectedHomepageTitle = "Manage Your Team’s Projects From Anywhere | Trello";
-								if (actualHomepageTitle.equals(expectedHomepageTitle)) {
-									System.out.println("Scenario sucessfully executed");
-								} else {
-									System.out.println("Scenario execution failed");
-								}
-							} else {
-								System.out
-										.println("logoutPageActualTitle is not matching with logoutPageExpectedTitle");
-							}
-						} else {
-							System.out.println("actualChosenTitle is not matching with expectedChosenTitle");
-						}
-					} else {
-						System.out.println("createButton is disabled");
-					}
-				} else {
-					System.out.println("actualBoardsPageTitle is not matching with expectedBoardsPageTitle");
-				}
-			} else {
-				System.out.println("loginPageActualTitle is not matching with loginPageExpectedTitle");
-			}
+	            webdriverUtils.waitForATitle(driver, "Selenium | Trello");
+	            Assert.assertEquals(driver.getTitle(), "Selenium | Trello", "Topic page title does not match");
+
+	         // Creating lists
+	            webdriverUtils.waitTillElementToBeVisible(driver, topicPage.getListNameTextarea1());
+	            topicPage.getListNameTextarea1().sendKeys("Topics");
+	            topicPage.getSubmitButton().click();
+
+	            webdriverUtils.waitTillElementToBeVisible(driver, topicPage.getListNameTextarea2());
+	            topicPage.getListNameTextarea2().sendKeys("Manual");
+	            topicPage.getSubmitButton().click();
+
+	            webdriverUtils.waitTillElementToBeVisible(driver, topicPage.getListNameTextarea3());
+	            topicPage.getListNameTextarea3().sendKeys("SQL");
+	            topicPage.getSubmitButton().click();
+
+	            webdriverUtils.waitTillElementToBeVisible(driver, topicPage.getListNameTextarea4());
+	            topicPage.getListNameTextarea4().sendKeys("Java");
+	            topicPage.getSubmitButton().click();
+
+	            webdriverUtils.waitTillElementToBeClickable(driver, topicPage.getCloseIcon());
+	            topicPage.getCloseIcon().click();
+
+	            // Adding cards
+	            webdriverUtils.waitTillElementToBeClickable(driver, topicPage.getAddCardButton());
+	            topicPage.getAddCardButton().click();
+
+	            webdriverUtils.waitTillElementToBeVisible(driver, topicPage.getCardTitleTextarea());
+	            topicPage.getCardTitleTextarea().sendKeys("Test Cases");
+	            topicPage.getAddCardSubmitButton().click();
+
+	            topicPage.getAddCardButton().click();
+	            topicPage.getCardTitleTextarea().sendKeys("Agile Methodology");
+	            topicPage.getAddCardSubmitButton().click();
+
+	            topicPage.getAddCardButton().click();
+	            topicPage.getCardTitleTextarea().sendKeys("Joins");
+	            topicPage.getAddCardSubmitButton().click();
+
+	            topicPage.getAddCardButton().click();
+	            topicPage.getCardTitleTextarea().sendKeys("Normalization");
+	            topicPage.getAddCardSubmitButton().click();
+
+	            topicPage.getAddCardButton().click();
+	            topicPage.getCardTitleTextarea().sendKeys("OOPS");
+	            topicPage.getAddCardSubmitButton().click();
+
+	            topicPage.getAddCardButton().click();
+	            topicPage.getCardTitleTextarea().sendKeys("Collection");
+	            topicPage.getAddCardSubmitButton().click();
+
+	            webdriverUtils.waitTillElementToBeClickable(driver, topicPage.getCloseIcon());
+	            topicPage.getCloseIcon().click();
+
+	            // Perform click and hold actions
+	            Actions actions = new Actions(driver);
+
+	            webdriverUtils.waitTillElementToBeClickable(driver, topicPage.getTestCaseTopic());
+	            webdriverUtils.waitTillElementToBeClickable(driver, topicPage.getManualTextField());
+	            actions.clickAndHold(topicPage.getTestCaseTopic()).moveToElement(topicPage.getManualTextField()).release().perform();
+
+	            webdriverUtils.waitTillElementToBeClickable(driver, topicPage.getAgileTopic());
+	            actions.clickAndHold(topicPage.getAgileTopic()).moveToElement(topicPage.getManualTextField()).release().perform();
+
+	            webdriverUtils.waitTillElementToBeClickable(driver, topicPage.getJoinsTopic());
+	            webdriverUtils.waitTillElementToBeClickable(driver, topicPage.getSqlTextField());
+	            actions.clickAndHold(topicPage.getJoinsTopic()).moveToElement(topicPage.getSqlTextField()).release().perform();
+
+	            webdriverUtils.waitTillElementToBeClickable(driver, topicPage.getNormalizationTopic());
+	            actions.clickAndHold(topicPage.getNormalizationTopic()).moveToElement(topicPage.getSqlTextField()).release().perform();
+
+	            webdriverUtils.waitTillElementToBeClickable(driver, topicPage.getOopsTopic());
+	            webdriverUtils.waitTillElementToBeClickable(driver, topicPage.getJavaTextField());
+	            actions.clickAndHold(topicPage.getOopsTopic()).moveToElement(topicPage.getJavaTextField()).release().perform();
+
+	            webdriverUtils.waitTillElementToBeClickable(driver, topicPage.getCollectionTopic());
+	            actions.clickAndHold(topicPage.getCollectionTopic()).moveToElement(topicPage.getJavaTextField()).release().perform();
+
+	            System.out.println("Click and Hold Successfully Performed");
+
+	            // Log out
+	            webdriverUtils.waitTillElementToBeClickable(driver, boardsPage.getMenuIcon());
+	            boardsPage.getMenuIcon().click();
+
+	            webdriverUtils.waitTillElementToBeClickable(driver, boardsPage.getLogoutLink());
+	            boardsPage.getLogoutLink().click();
+
+	            webdriverUtils.waitForATitle(driver, "Log out of your Atlassian account - Log in with Atlassian account");
+	            Assert.assertEquals(driver.getTitle(), "Log out of your Atlassian account - Log in with Atlassian account",
+	                    "Logout page title does not match");
+
+	            webdriverUtils.waitTillElementToBeClickable(driver, logoutPage.getLogoutButton());
+	            logoutPage.getLogoutButton().click();
+
+	            webdriverUtils.waitForATitle(driver, "Manage Your Team’s Projects From Anywhere | Trello");
+	            Assert.assertEquals(driver.getTitle(), "Manage Your Team’s Projects From Anywhere | Trello",
+	                    "Welcome page title does not match");
 
 		} catch (Exception e) {
 			e.printStackTrace();
